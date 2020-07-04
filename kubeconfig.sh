3f286eb88e4acc1b5f40da38fa3ac13a23ed625d
iceworld <gosoumei@hotmail.com>
2020-07-04 11:23:14 +0800

    Update kubeconfig.sh

--- kubeconfig.sh
+++ kubeconfig.sh
@@ -2,7 +2,7 @@ CCP_HOST='10.10.20.110'
 CCP_PASSWORD='Cisco123'
 curl -X POST --header 'Content-Type: application/x-www-form-urlencoded' --header 'Accept: application/json' -d "username=admin&password=$CCP_PASSWORD" "https://$CCP_HOST/2/system/login/" -k -c cookie.txt
 #grab cluster uuid
-CLUSTER_UUID=$(curl -sk -b cookie.txt -H "Content-Type:application/json" https://$CCP_HOST/2/clusters/ | jq '.[].uuid')
-curl -sk -b cookie.txt https://$CCP_HOST/2/clusters/$(eval echo $CLUSTER_UUID)/env -o kubeconfig.yaml
-echo "kubeconfig file is saved to  kubeconfig.yaml"
+CLUSTER_UUID=$(curl -sk -b cookie.txt -H "Content-Type:application/json" https://$CCP_HOST/2/clusters/ | jq '.[].uuid') | sed "s/^\([\"']\)\(.*\)\1\$/\2/g"
+curl -sk -b cookie.txt https://$CCP_HOST/2/clusters/$CLUSTER_UUID/env -o kubeconfig.yaml
+echo "kubeconfig file is saved to  kubeconfig.yaml, use export KUBECONFIG=./kubeconfig.yaml to refer it"
 export KUBECONFIG=./kubeconfig.yaml
